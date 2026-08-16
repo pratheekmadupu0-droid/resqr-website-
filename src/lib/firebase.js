2cf8cb1bd081b51a45bbb6b5d5a136061ffa8ce7
pratheekmadupu <pratheekmadupu2006@gmail.com>
2026-08-17 02:51:24 +0530

    config: update firebase authDomain to https://www.resqr.co.in/

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,7 +10,7 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBFXvSwZSrMbYHQkrge6UyZOv2uxU0VkPA",
-    authDomain: "resqr.co.in",
+    authDomain: "https://www.resqr.co.in/",
     databaseURL: "https://emergency-qr-b0adf-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "emergency-qr-b0adf",
     storageBucket: "emergency-qr-b0adf.firebasestorage.app",
